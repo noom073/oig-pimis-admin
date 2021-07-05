f3b2635a66eb20021759df00c8616a915005b787
noom073 <noom073@hotmail.com>
2021-07-05 21:42:20 +0700

    update manage gallery feature

--- application/views/auditor/inspect/script.php
+++ application/views/auditor/inspect/script.php
@@ -52,7 +52,7 @@
                         questions += `<div class="pl-3 border-left my-2 question">
                                     <div>- ${question.Q_NAME} ?</div>
                                     <div class="pl-5 col-2">
-                                        <input class="auditor-score form-control" type="number" name="score-${question.Q_ID}" value="${question.SCORE}" max="${question.LIMIT_SCORE}" min="0">   
+                                        <input class="auditor-score form-control" type="number" name="score-${question.Q_ID}" value="${question.SCORE}" max="${question.LIMIT_SCORE}" min="0" step="0.01">   
                                         <small class="text-danger">คะแนนสูงสุดไม่เกิน ${textMaxDesc}</small>
                                     </div>
                                 </div>`;
@@ -94,7 +94,7 @@
                         html += `<div class="pl-3 border-left my-2 question">
                                     <div>- ${question.Q_NAME} ?</div>
                                     <div class="pl-5 col-2">
-                                        <input class="auditor-score form-control" type="number" name="score-${question.Q_ID}" value="${question.SCORE}" max="${question.LIMIT_SCORE}" min="0">   
+                                        <input class="auditor-score form-control" type="number" name="score-${question.Q_ID}" value="${question.SCORE}" max="${question.LIMIT_SCORE}" min="0" step="0.01">   
                                         <small class="text-danger">คะแนนสูงสุดไม่เกิน ${textMaxDesc}</small>
                                     </div>
                                 </div>`;
